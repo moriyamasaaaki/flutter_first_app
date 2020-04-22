13f4594aef63d8aae1d45c65003bcf1f539817b8
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-22 15:47:09 +0900

    refactor(dart): コード修正、コンポーネント分割

--- lib/screens/welcom.screen.dart
+++ lib/screens/welcom.screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutterfirstapp/main.dart';
 import 'package:flutterfirstapp/screens/login.screen.dart';
 import 'package:flutterfirstapp/screens/register.screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import '../components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -83,33 +83,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               },
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  RoundedButton({this.color, this.title, @required this.onPressed});
-
-  final Color color;
-  final String title;
-  final Function onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(3.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            title,
-          ),
         ),
       ),
     );
